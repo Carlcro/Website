2d0222c413e24bad76c3fb9f50fbf7ff7c38dd5e
Carl Cronsioe <Carl.cronsioe@gmail.com>
2017-07-27 21:45:12 +0200

    Ready to start Chapter 5

--- mysite/lists/tests.py
+++ mysite/lists/tests.py
@@ -7,3 +7,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')  #2
         self.assertEqual(found.func, home_page)  #3
+
+    def test_home_page_returns_correct_hotl(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))  #3
+        self.assertIn(b'<title>To-Do lists</title>', response.content)  #4
+        self.assertTrue(response.content.endswith(b'</html>'))  #5
